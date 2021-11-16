ee1dbad991ecd1ee0df27e321a4a0deb493bc3ef
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2021-11-16 15:36:35 +0100

    include eigen3, update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BenignOverfitting
-from Seminar of Mathematics and Numeric of Deep Neural Networks for Physical Simulation
+Seminar of Mathematics and Numerics of Deep Neural Networks for Physical Simulations
+
+## libraries
+The implementation uses eigen3. So eigen3 muss be on the machine to compile the code.
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <random>
-#include <Eigen/Core>
+#include <eigen3/Eigen/Core>
 
+/*!
+ * \brief Samples struct stores all sample datas x_i, y_i
+ */
 struct Samples {
-  // vectors x_i and scalars y_i
+  Eigen::MatrixXf X;
+  Eigen::VectorXf Y;
 };
+
+Samples generateSamples(unsigned int n, unsigned int k) {
+  Eigen::MatrixXf x(k, n);
+  Eigen::VectorXf y(n);
+
+  return Samples{x, y};
+}
 
 int main(int argc, char* argv[]) {
   return 0;
