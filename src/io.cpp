e627c226a7b276f3d56f813170393ce7819cc886
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2021-11-29 23:33:11 +0100

    write prediction function for mnist

--- src/io.cpp
+++ src/io.cpp
@@ -32,22 +32,22 @@ static Data read(const char* filename, size_t count) {
 }
 
 Data readTrainImages(size_t imagecount) {
-  assert(imagecount < TRAINING_SET_SIZE && "Number of training samples to read should not exeed data size!");
+  assert(imagecount <= TRAINING_SET_SIZE && "Number of training samples to read should not exeed data size!");
   return read(TRAIN_IMAGE_FILE, imagecount * PIXELS_PER_IMAGE);
 }
 
 Data readTrainLables(size_t labelcount) {
-  assert(labelcount < TRAINING_SET_SIZE && "Number of training samples to read should not exeed data size!");
+  assert(labelcount <= TRAINING_SET_SIZE && "Number of training samples to read should not exeed data size!");
   return read(TRAIN_LABEL_FILE, labelcount);
 }
 
 Data readTestImages(size_t imagecount) {
-  assert(imagecount < TEST_SET_SIZE && "Number of test samples to read should not exeed data size!");
+  assert(imagecount <= TEST_SET_SIZE && "Number of test samples to read should not exeed data size!");
   return read(TEST_IMAGE_FILE, imagecount * PIXELS_PER_IMAGE);
 }
 
 Data readTestLables(size_t labelcount) {
-  assert(labelcount < TEST_SET_SIZE && "Number of test samples to read should not exeed data size!");
+  assert(labelcount <= TEST_SET_SIZE && "Number of test samples to read should not exeed data size!");
   return read(TEST_LABEL_FILE, labelcount);
 }
 }  // namespace MNIST
--- src/mnist.cpp
+++ src/mnist.cpp
@@ -14,45 +14,89 @@
 #include "io.hpp"
 #include "types.hpp"
 
-using Matrix_X = Eigen::Matrix<float, MNIST::PIXELS_PER_IMAGE, Eigen::Dynamic>;
-using Vector_Y = Eigen::Matrix<float, Eigen::Dynamic, MNIST::POSSIBLE_LABELS>;
+using Matrix_X      = Eigen::Matrix<float, MNIST::PIXELS_PER_IMAGE, Eigen::Dynamic>;
+using Matrix_Y      = Eigen::Matrix<float, Eigen::Dynamic, MNIST::POSSIBLE_LABELS>;
+using Matrix_theta  = Eigen::Matrix<float, MNIST::PIXELS_PER_IMAGE, MNIST::POSSIBLE_LABELS>;
+using VectorLabelsf = Eigen::Matrix<float, 1, MNIST::POSSIBLE_LABELS>;
 
 namespace MNIST {
-Matrix_X buildMatrix_X(const size_t n) {
-  Data images = readTrainImages(n);
-  Matrix_X x(PIXELS_PER_IMAGE, n);
-  assert((images.size = PIXELS_PER_IMAGE * n) && "Number of bytes read from file does not fit.");
+Matrix_X buildMatrix_X(const size_t n, const Data images) {
+  assert((images.size == PIXELS_PER_IMAGE * n) && "Number of bytes read from file does not fit.");
 
+  Matrix_X x(PIXELS_PER_IMAGE, n);
   for (size_t j = 0; j < n; ++j) {
     for (size_t i = 0; i < PIXELS_PER_IMAGE; ++i) {
       x(i, j) = (float) images.data[PIXELS_PER_IMAGE * j + i] / MAXBYTE;
     }
   }
-  free(images.data);
+
   return x;
 }
 
-Vector_Y buildVector_Y(const size_t n) {
-  Data labels = readTrainLables(n);
+Matrix_Y buildMatrix_Y(const size_t n, const Data labels) {
   assert(labels.size == n && "Number of labels read and n are missmatching!");
 
-  Vector_Y y(n, POSSIBLE_LABELS);
-
+  Matrix_Y y(n, POSSIBLE_LABELS);
   for (size_t i = 0; i < n; ++i) {
     for (unsigned char j = 0; j < POSSIBLE_LABELS; ++j) {
       y(i, j) = (j == labels.data[i]) ? 1.0f : 0.f;
     }
   }
-  free(labels.data);
   return y;
 }
 
+static size_t maxIndex(const VectorLabelsf vec) {
+  assert(vec.cols() != 0 && "Vector should be non empty!");
+  size_t index = 0;
+  float max    = vec(0);
+  for (size_t i = 1; i < vec.cols(); ++i) {
+    if (vec(i) > max) {
+      max   = vec(i);
+      index = i;
+    }
+  }
+  return index;
+}
+
+float predict(const size_t t, const Matrix_theta theta) {
+  Data images = readTestImages(t);
+  Data labels = readTestLables(t);
+  Matrix_X x  = buildMatrix_X(t, images);
+  Matrix_Y y  = buildMatrix_Y(t, labels);
+  free(images.data);
+  free(labels.data);
+
+  Matrix_Y y_predict = x.transpose() * theta;
+  std::cerr << y - y_predict << "\n";
+  size_t counter = 0;
+  for (size_t i = 0; i < t; ++i) {
+    if (y(maxIndex(y_predict.row(i))) == 1.0f) {
+      ++counter;
+      std::cerr << i << " ";
+    }
+  }
+  std::cerr << "\n";
+  return (float) counter / t;
+}
+
 void mnist(int argc, char* argv[]) {
-  if (argc < 3) {
+  if (argc < 4) {
     throw std::runtime_error("Too less arguments!");
   }
   const size_t n = atoi(argv[2]);
-  std::cerr << buildMatrix_X(n).transpose() << "\n";
-  std::cerr << buildVector_Y(n) << "\n";
+  const size_t t = atoi(argv[3]);
+  Data images    = readTrainImages(n);
+  Data labels    = readTrainLables(n);
+  Matrix_X x     = buildMatrix_X(n, images);
+  Matrix_Y y     = buildMatrix_Y(n, labels);
+  free(images.data);
+  free(labels.data);
+
+  // solve the system of equations
+  Eigen::CompleteOrthogonalDecomposition<Eigen::MatrixXf> cQR(x.transpose());
+  Matrix_theta theta = cQR.solve(y);
+  float accuracy     = predict(t, theta);
+
+  std::cerr << "Accuracy: " << accuracy << "\n";
 }
 }  // namespace MNIST
